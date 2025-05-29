4a990e93e5ff1193197ca87a12b972006414be9b
mintsky0172 <mintsky0172@naver.com>
2025-05-29 18:51:40 +0900

    250529

---
+++ client/chapter/core/02.variables.js
@@ -0,0 +1,49 @@
+/* ------------------ */
+/* Variables          */
+/* ------------------ */
+
+let admin;
+let name;
+
+name = 'John';
+
+admin = name;
+
+console.log(admin);
+
+/* -------------------------------------------------------------------------- */
+
+let OUR_PLANET_NAME = 'EARTH';
+
+let userName = 'John';
+
+/* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
+
+// - 갯수 별 상품 가격 계산하기
+let productCount = 5;
+const productPrice = 30000;
+
+// - 구매 제품 가격의 총 합
+let totalPrice = productCount * productPrice;
+
+// - 1년 기준 일(day)자 수
+const numberOfDay = 365;
+
+// - 구매 결제 여부
+let onPurchased = true;
+
+// - 구매 결제 내역
+let purchasedList;
+
+// - 브랜드 접두사
+const brandPrefix = 'Siru';
+
+// - 오늘의 운세
+const todaysFortune = 'lucky';
+
+// - 상품 정보
+const productInformation = 'aaa';
+
+/* variables ----------------------------------------------------------- */
+
+/* constant variables -------------------------------------------------- */
