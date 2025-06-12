4fe0884f3906b6eabe6b7ccffba30c3fe7045817
mintsky0172 <mintsky0172@naver.com>
2025-06-12 19:26:28 +0900

    0612

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,20 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs}"],
+    files: ['**/*.{js,mjs,cjs}'],
     plugins: { js },
-    extends: ["js/recommended"],
+    extends: ['js/recommended'],
   },
   {
-    files: ["**/*.{js,mjs,cjs}"],
+    files: ['**/*.{js,mjs,cjs}'],
     languageOptions: {
-      globals: { ...globals.browser, ...globals.node },
+      globals: { ...globals.browser, ...globals.node, getNode: true },
     },
     rules: {
-      "no-unused-vars": "off",
+      'no-unused-vars': 'off',
     },
   },
 ]);
